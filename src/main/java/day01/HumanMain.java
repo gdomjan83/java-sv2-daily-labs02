baeec9251633202265b23efca136c88d10470178
J_C <gomjan83@gmail.com>
2021-10-19 17:02:22 +0200

    Week02/Day01 project finished.

---
+++ src/main/java/day01/HumanMain.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class HumanMain {
+    public static void main(String[] args) {
+        Human human = new Human("Gábor", 38);
+
+        System.out.println(human.getName());
+        System.out.println(human.getAge());
+    }
+}
